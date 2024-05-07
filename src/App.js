e47bfcfaa4f70b16a96a6ff6972c3e79396f9299
john7319 <jonniekaras@gmail.com>
2024-05-07 19:36:41 +0300

    Added a Component folder, About.js file and a navbar, passed in the components in the index.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
+import NavBar from './Components/NavBar';
 
 function App() {
   return (
@@ -17,7 +19,12 @@ function App() {
         >
           Learn React
         </a>
+        <NavBar />
+
       </header>
+      <body>
+        <About />
+      </body>
     </div>
   );
 }
