046caaa40192c90ce3dccb66c3344da1f1af24f4
john7319 <jonniekaras@gmail.com>
2024-05-09 11:27:29 +0300

    Restored the routes page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
+import ArtCard from './Card';
+import NavBar from './Components/NavBar';
+import { Searchbar } from './Components/Searchbar';
 
 function App() {
-  const [results, setResults] = useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <NavBar/>
       </header>
+      <body>
+        <Searchbar/>
+        <ArtCard/>
+      </body>
     </div>
   );
 }
