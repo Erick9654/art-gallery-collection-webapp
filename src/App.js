d3f33fc095e041ba8de4c598d3bc3366ad1a9cb5
Cheruiyot Robert <kiruir38@gmail.com>
2024-05-09 14:37:23 +0300

    Card

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-
 import './App.css';
+import React, {useState} from 'react';
 import ArtCard from './Card';
 import NavBar from './Components/NavBar';
 import { Searchbar } from './Components/Searchbar';
+import {SearchResultsList} from './Components/SearchResultsList'
+
 
 function App() {
+  const [results, setResults] =useState ([])
+
+  function handleDelete(art) {
+    console.log("Deleting art:", art);
+  };
   return (
     <div className="App">
       <header>
-        <NavBar/>
+        <NavBar />
       </header>
       <body>
-        <Searchbar/>
-        <ArtCard/>
+      <div className="search-bar-container">
+            <Searchbar setResults={setResults}/>
+            <SearchResultsList results={results}/>
+        </div>
+        <ArtCard onDelete={handleDelete}/>
+        
       </body>
     </div>
   );
