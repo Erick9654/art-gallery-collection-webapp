fb66a853b8951b3fa64cc88f3571fbf4cf13b52d
Carl Ngovi <carl.ngovi@student.moringaschool.com>
2024-05-09 12:13:20 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -8,20 +8,7 @@ function App() {
   const [results, setResults] = useState([]);
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      
        <div className="Search">
         <div className="search-bar-container">
             <Searchbar setResults={setResults}/>
