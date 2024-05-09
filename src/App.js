6cde72a69bb38c532f92ab733525158f9ad4a707
Cheruiyot Robert <kiruir38@gmail.com>
2024-05-09 11:45:00 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import { useState } from "react";
-import { Searchbar } from './Components/Searchbar';
-import { SearchResultsList } from './Components/SearchResultsList';
-import NavBar from './Components/NavBar';
 
 function App() {
   const [results, setResults] = useState([]);
   return (
     <div className="App">
-      <header>
-        <NavBar/>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
-      <body>
-        <div className="Search">
-          <div className="search-bar-container">
-              <Searchbar setResults={setResults}/>
-              <SearchResultsList results={results}/>
-              
-          </div>
-      </div>
-    </body>
     </div>
   );
 }
